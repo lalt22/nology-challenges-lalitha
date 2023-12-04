1f58115f25d370d4c906ba06bd6cf22a54bb574a
lalt22 <lalitha2seshadri@gmail.com>
2023-12-04 11:40:00 +1100

    Added note for number string operation rules

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log(typeof 15);
+console.log(typeof 5.5);
+console.log(typeof NaN);
+console.log(typeof (1 !=2));
+
+console.log("hamburger" + "s");
+
+//NOTE: Addition symbol will concatenate numbers expressed in strings:
+/*
+    "1" + "1" = 11
+    "1" + 1 = 11
+    1 + 1 = 2
+*/
+console.log("1" + "3");
+
+//All other operations (-, /, *) will do the numeric operation
+/*
+    "1" - "3" = -2
+    "1" - 3 = -2
+*/
+console.log("1" - "3");
+console.log("johnny" - 5);
+console.log(99 * "luftbaloons")
